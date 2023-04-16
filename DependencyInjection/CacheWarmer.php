12f35f66e713d79c0fd90a60fd12b68db935fabc
Marco Meyer <marco.meyer@glitchr.io>
2023-04-16 10:45:43 +0900

    Initial commit

---
+++ DependencyInjection/CacheWarmer.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Hypertext\Access\DependencyInjection;
+
+use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
+use Hypertext\Access\Factory\HypertextAccessFactory;
+
+class CacheWarmer implements CacheWarmerInterface
+{
+    /**
+     * @var string
+     */
+    protected string $shellVerbosity;
+
+    /**
+     * @var HypertextAccessFactory
+     */
+    protected $wellKnownFactory;
+
+    public function __construct(HypertextAccessFactory $wellKnownFactory)
+    {
+        $this->shellVerbosity = getenv("SHELL_VERBOSITY");
+        $this->wellKnownFactory   = $wellKnownFactory;
+    }
+
+    public function isOptional(): bool
+    {
+        return true;
+    }
+    public function warmUp($cacheDir): array
+    {
+        if ($this->shellVerbosity > 0 && php_sapi_name() == "cli") {
+            echo " // Warming up cache... Well Known Bundle.. ";
+        }
+
+        $robots = $this->wellKnownFactory->robots();
+        if ($this->shellVerbosity > 0 && php_sapi_name() == "cli" && $robots) {
+            echo "robots.txt.. ";
+        }
+
+        $security = $this->wellKnownFactory->security();
+        if ($this->shellVerbosity > 0 && php_sapi_name() == "cli" && $security) {
+            echo "security.txt.. ";
+        }
+
+        $humans = $this->wellKnownFactory->humans();
+        if ($this->shellVerbosity > 0 && php_sapi_name() == "cli" && $humans) {
+            echo "humans.txt.. ";
+        }
+
+        $ads = $this->wellKnownFactory->ads();
+        if ($this->shellVerbosity > 0 && php_sapi_name() == "cli" && $ads) {
+            echo "ads.txt.. ";
+        }
+
+        $htaccess = $this->wellKnownFactory->htaccess();
+        if ($this->shellVerbosity > 0 && php_sapi_name() == "cli" && $htaccess) {
+            echo ".htaccess.. ";
+        }
+
+        if ($this->shellVerbosity > 0 && php_sapi_name() == "cli") {
+            echo PHP_EOL.PHP_EOL;
+        }
+
+        return array_filter([$security, $robots, $humans, $ads, $htaccess]);
+    }
+}
---
+++ HypertextAccessBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Hypertext\Access;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HypertextAccessBundle extends Bundle
+{
+}
