99b2ef877e4be8ddf3b897bfbaa40c674372d4ef
Marco Meyer <marco.meyer@glitchr.io>
2024-02-12 09:57:47 +0900

    update cache warmer

--- DependencyInjection/CacheWarmer.php
+++ DependencyInjection/CacheWarmer.php
@@ -27,7 +27,7 @@ class CacheWarmer implements CacheWarmerInterface
     {
         return false;
     }
-    public function warmUp($cacheDir): array
+    public function warmUp(string $cacheDir, ?string $buildDir = null): array
     {
         if ($this->shellVerbosity > 0 && php_sapi_name() == "cli") {
             echo " // Warming up cache... Hypertext access bundle.. ";
